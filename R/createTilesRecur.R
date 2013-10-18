6e1f427e7539b97574be54907f839c705085c17c
TJ McKinley <tjm44@cam.ac.uk>
2013-10-18 14:16:35 +0100

    Amended file path generation to make more general than just Linux.

--- R/createTilesRecur.R
+++ R/createTilesRecur.R
@@ -36,15 +36,15 @@ createTilesRecur <- function(object, start_coords, zoom, title, max_zoom, tms)
 		if(tms == TRUE) coords[, 2] <- (2 ^ zoom) - coords[, 2] - 1
 	
 		#create directories based on initial tiles
-		dir.create(paste0(title, "/", zoom))
+		dir.create(file.path(title, zoom))
 		#create sub-directories based on x- and y-coordinates
-		for(i in unique(coords[, 1])) dir.create(paste(c(title, zoom, i), collapse = "/"))
+		for(i in unique(coords[, 1])) dir.create(file.path(title, zoom, i))
 	
 		#now plot into directories
 		for(i in 1:nrow(coords))
 		{
-			pathtofile <- paste(c(title, zoom, coords[i, 1]), collapse = "/")
-			png(paste0(pathtofile, "/", coords[i, 2], ".png"), width = 248, height = 248, bg = 'transparent')
+			pathtofile <- file.path(title, zoom, coords[i, 1])
+			png(file.path(pathtofile, paste0(coords[i, 2], ".png")), width = 248, height = 248, bg = 'transparent')
 			par(mar = c(0, 0, 0, 0), xaxs = "i", yaxs = "i")
 			plot(object, xlim = tiles[i, 1:2], ylim = tiles[i, 3:4])
 			dev.off()
--- R/createTilesWorker.R
+++ R/createTilesWorker.R
@@ -14,9 +14,9 @@ createTilesWorker <- function(object, start_tiles, start_zoom, max_zoom, title, 
 	#create output directory
 	dir.create(title)
 	#create directories based on initial tiles
-	dir.create(paste0(title, "/", start_zoom))
+	dir.create(file.path(title, start_zoom))
 	#create sub-directories based on x- and y-coordinates
-	for(i in unique(start_coords[, 1]))	dir.create(paste(c(title, start_zoom, i), collapse = "/"))
+	for(i in unique(start_coords[, 1]))	dir.create(file.path(title, start_zoom, i))
 	
 	#convert coordinates to TMS if required
 	if(tms == TRUE)
@@ -28,8 +28,8 @@ createTilesWorker <- function(object, start_tiles, start_zoom, max_zoom, title, 
 	#now plot into directories
 	for(i in 1:nrow(start_coords))
 	{
-		pathtofile <- paste(c(title, start_zoom, start_coords[i, 1]), collapse = "/")
-		png(paste0(pathtofile, "/", start_coords[i, 2], ".png"), width = 248, height = 248, bg = 'transparent')
+		pathtofile <- file.path(title, start_zoom, start_coords[i, 1])
+		png(file.path(pathtofile, paste0(start_coords[i, 2], ".png")), width = 248, height = 248, bg = 'transparent')
 		par(mar = c(0, 0, 0, 0), xaxs = "i", yaxs = "i")
 		plot(object, xlim = start_tiles[i, 1:2], ylim = start_tiles[i, 3:4])
 		dev.off()
